029ce93e706c1eef764d34fd1c81dc621026c52a
OmarAlanazi <oalanazi@villacapital.sa>
2025-09-23 19:30:05 +0300

    override CreateAsync for loans

--- LibrarySystem/Services/ILoansService.cs
+++ LibrarySystem/Services/ILoansService.cs
@@ -11,8 +11,24 @@ public interface ILoansService : ICrudService<ILoansRepository, LoansEntity, Loa
 
 }
 
-public class LoansService(ILoansRepository loansRepository, IDbUnitOfWork dbUnitOfWork, IMapper mapper) : 
+public class LoansService(ILoansRepository loansRepository, IBooksRepository booksRepository, IMembersRepository membersRepository, IDbUnitOfWork dbUnitOfWork, IMapper mapper) : 
     CrudService<ILoansRepository, LoansEntity, LoansDto, CreateLoanRequestDto>(loansRepository, dbUnitOfWork, mapper), ILoansService 
 {
-    
+    public override async Task<LoansDto> CreateAsync(CreateLoanRequestDto request, CancellationToken ct = default)
+    {
+        var loanEntity = mapper.Map<LoansEntity>(request);
+        
+        var bookEntity = await booksRepository.GetByIdAsync(request.BookId, false, ct) ?? throw new KeyNotFoundException();
+        var memberEntity = await membersRepository.GetByIdAsync(request.MemberId, false, ct) ?? throw new KeyNotFoundException();
+
+        loanEntity.BookId = bookEntity.Id;
+        loanEntity.MemberId = memberEntity.Id;
+            
+        // TODO: add a check if the book is already token or not 
+        
+        await loansRepository.AddAsync(loanEntity, ct);
+        await dbUnitOfWork.SaveChangesAsync(ct);
+        
+        return mapper.Map<LoansDto>(loanEntity);
+    }
 }
