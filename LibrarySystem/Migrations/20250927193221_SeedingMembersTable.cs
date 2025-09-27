ee647a76e38332c4f715a2c93c5970778bcadb37
OmarAlanazi <oalanazi@villacapital.sa>
2025-09-27 22:34:06 +0300

    seeding members with migrations

---
+++ LibrarySystem/Migrations/20250927193221_SeedingMembersTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibrarySystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingMembersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO members (name, email, created_at, updated_at, key)
+                VALUES
+                ('Alice Johnson', 'alice@example.com', NOW(), NOW(), gen_random_uuid()::text),
+                ('Bob Smith',     'bob@example.com',   NOW(), NOW(), gen_random_uuid()::text),
+                ('Charlie Lee',   'charlie@example.com', NOW(), NOW(), gen_random_uuid()::text);
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM members
+                WHERE email IN (
+                    'alice@example.com',
+                    'bob@example.com',
+                    'charlie@example.com'
+                );
+            ");
+        }
+    }
+}
